29de32e7c5a85ff4a88f2eaa57e532112ebd1db0
s3h10r <sven.hessenmueller@gmail.com>
2019-08-02 18:54:51 +0200

    migrates filter to new-style (class-based) #10

--- egw_plugin_quads.py
+++ egw_plugin_quads.py
@@ -1,62 +1,42 @@
-#!/usr/bin/env python
-#coding=utf-8
-# === einguteswerkzeug plugin-interface ===
-# --- all einguteswerkzeug-plugins (generators, filters) must implement this
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+egw-filter Quads
+"""
 import logging
 import random
 import string
-
-name = "quads"
-description = "based on https://github.com/fogleman/Quads"
-kwargs = {  'image' : None,
-            'mode' : random.randint(1,3),
-            'iterations' : 1024,
-            'leaf_size'  : 4,
-            'padding'    : 1,
-            'fill_color' : (255,255,255),
-            'error_rate' : 0.5,
-            'area_power' : 0.25,
-            'output_scale' : 1,
-         }
-args = None
-author = "Michael Fogleman,"
-version = "0.1.0"
-
-def run(**kwargs):
-    """
-    this is the interface/wrapper around the functionality of the plugin.
-    """
-    return quads_main(**kwargs)
-# --- END all einguteswerkzeug-plugins (generators, filters) must implement this
-
-def get_plugin_doc(format='text'):
-    """
-    """
-    if format not in ('txt', 'text', 'plaintext'):
-        raise Exception("Sorry. format %s not available. Valid options are ['text']" % format)
-    tpl_doc = string.Template("""
-    filters.$name - $description
-    kwargs  : $kwargs
-    args    : $args
-    author  : $author
-    version : __version__
-    """)
-    return tpl_doc.substitute({
-        'name' : name,
-        'description' : description,
-        'kwargs' : kwargs,
-        'args'    : args,
-        'author'  : author,
-        'version' : __version__,
-        })
-
-if __name__ == '__main__':
-    print(get_plugin_doc())
-
-# === END einguteswerkzeug plugin-interface
-
-# --- .. here comes the plugin-specific part to get some work done...
-def _somework(arg1=None, arg2 = None):
-    return ("Hello from Plugin dummy1. arg1=%s, arg2=%s " % (arg1,arg2))
-
+import sys
+from PIL import Image
+from einguteswerkzeug.plugins import EGWPluginFilter
 from .main import main as quads_main
+
+fmeta = {
+    "name" : "quads",
+    "version" : "0.1.2",
+    "description" : "based on https://github.com/fogleman/Quads",
+    "author" : "Michael Fogleman"
+}
+
+class Quads(EGWPluginFilter):
+    def __init__(self, **kwargs):
+        super().__init__(**fmeta)
+        # defining mandatory kwargs (addionals to the mandatory of the base-class)
+        add_kwargs = {  'mode' : random.randint(1,3),
+                        'iterations' : 1024,
+                        'leaf_size'  : 4,
+                        'padding'    : 1,
+                        'fill_color' : (255,255,255),
+                        'error_rate' : 0.5,
+                        'area_power' : 0.25,
+                        'output_scale' : 1,
+                        }
+        self._define_mandatory_kwargs(self, **add_kwargs)
+        self.kwargs = kwargs
+
+    def run(self):
+        return quads_main(**self._kwargs)
+
+
+filter = Quads()
+assert isinstance(filter,EGWPluginFilter)
